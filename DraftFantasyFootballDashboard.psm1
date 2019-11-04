f6b114fbe9f955c897aa31ac9b0b88fffc3ec4fe
sk82jack <sk82jack@users.noreply.github.com>
2019-11-04 01:51:45 +0000

    add https

--- DraftFantasyFootballDashboard.psm1
+++ DraftFantasyFootballDashboard.psm1
@@ -45,8 +45,9 @@ function Start-Dashboard {
         $Cache:CupInfo = Get-DraftCupInfo
     }
 
+    $Cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]::CreateFromCertFile('/var/cert/certificate.crt')
     try {
-        Start-UDDashboard -Dashboard $Dashboard -Port 8585 -Wait -Endpoint $EveryHour
+        Start-UDDashboard -Dashboard $Dashboard -Port 8585 -Wait -Endpoint $EveryHour -Certificate $Cert
     }
     catch {
         Write-Error -Exception $_.Exception

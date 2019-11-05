4864390e9d2a278e2c8a840ce7b51c7d23284a21
sk82jack <sk82jack@users.noreply.github.com>
2019-11-05 02:48:15 +0000

    generate pfx file and import it

--- DraftFantasyFootballDashboard.psm1
+++ DraftFantasyFootballDashboard.psm1
@@ -45,7 +45,9 @@ function Start-Dashboard {
         $Cache:CupInfo = Get-DraftCupInfo
     }
 
-    $Cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]::CreateFromCertFile('/var/cert/certificate.crt')
+    openssl pkcs12 -export -out certificate.pfx -inkey /var/cert/private.key -in /var/cert/certificate.crt -certfile /var/cert/ca_bundle.crt -passout pass:
+    $Path = Resolve-Path $PSScriptRoot/certificate.pfx
+    $Cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]::CreateFromCertFile($Path.Path)
     try {
         Start-UDDashboard -Dashboard $Dashboard -Port 8585 -Wait -Endpoint $EveryHour -Certificate $Cert
     }

1b87002506bf5cdcc7dd0b65d4208233311feac3
sk82jack <sk82jack@users.noreply.github.com>
2019-11-06 21:30:56 +0000

    change font colour in trades collapsibles

--- Pages/trades.ps1
+++ Pages/trades.ps1
@@ -4,17 +4,17 @@ New-UDPage -Name 'Trades' -Icon exchange_alt -Endpoint {
             New-UDTabContainer -Tabs {
                 New-UDTab -Text 'Prem' -Content {
                     New-UDCollapsible -Items {
-                        New-UDCollapsibleItem -Title 'Open Offers' -Content {
+                        New-UDCollapsibleItem -Title 'Open Offers' -FontColor 'White' -Content {
                             New-UDGrid -Headers @('Type', 'Offered', 'Requested', 'Offered by', 'Offered to', 'Offer date') -DateTimeFormat "D/M/YY, H:mm" -NoExport -NoFilter -Properties @('Type', 'PlayersOut', 'PlayersIn', 'OutManager', 'InManager', 'createdAt') -Endpoint {
                                 $Cache:PremTrades | Where-Object Status -eq 'awaiting_response' | Out-UDGridData
                             }
                         }
-                        New-UDCollapsibleItem -Title 'History' -Content {
+                        New-UDCollapsibleItem -Title 'History' -FontColor 'White' -Content {
                             New-UDGrid -Headers @('Type', 'Offered', 'Requested', 'Offered by', 'Offered to', 'Offer date', 'Response date', 'Status') -DateTimeFormat "D/M/YY, H:mm" -NoExport -NoFilter -Properties @('Type', 'PlayersOut', 'PlayersIn', 'OutManager', 'InManager', 'createdAt', 'respondedAt', 'Status') -Endpoint {
                                 $Cache:PremTrades | Where-Object Status -ne 'awaiting_response' | Out-UDGridData
                             }
                         }
-                        New-UDCollapsibleItem -Title 'Waiver Order' -Content {
+                        New-UDCollapsibleItem -Title 'Waiver Order' -FontColor 'White' -Content {
                             New-UDTable -Headers 'Manager' -Style 'centered' -Content {
                                 $Cache:PremWaiver | Out-UDTableData -Property Manager
                             }
@@ -23,17 +23,17 @@ New-UDPage -Name 'Trades' -Icon exchange_alt -Endpoint {
                 }
                 New-UDTab -Text 'Freak' -Content {
                     New-UDCollapsible -Items {
-                        New-UDCollapsibleItem -Title 'Open Offers' -Content {
+                        New-UDCollapsibleItem -Title 'Open Offers' -FontColor 'White' -Content {
                             New-UDGrid -Headers @('Type', 'Offered', 'Requested', 'Offered by', 'Offered to', 'Offer date') -DateTimeFormat "D/M/YY, H:mm" -NoExport -NoFilter -Properties @('Type', 'PlayersOut', 'PlayersIn', 'OutManager', 'InManager', 'createdAt') -Endpoint {
                                 $Cache:FreakTrades | Where-Object Status -eq 'awaiting_response' | Out-UDGridData
                             }
                         }
-                        New-UDCollapsibleItem -Title 'History' -Content {
+                        New-UDCollapsibleItem -Title 'History' -FontColor 'White' -Content {
                             New-UDGrid -Headers @('Type', 'Offered', 'Requested', 'Offered by', 'Offered to', 'Offer date', 'Response date', 'Status') -DateTimeFormat "D/M/YY, H:mm" -NoExport -NoFilter -Properties @('Type', 'PlayersOut', 'PlayersIn', 'OutManager', 'InManager', 'createdAt', 'respondedAt', 'Status') -Endpoint {
                                 $Cache:FreakTrades | Where-Object Status -ne 'awaiting_response' | Out-UDGridData
                             }
                         }
-                        New-UDCollapsibleItem -Title 'Waiver Order' -Content {
+                        New-UDCollapsibleItem -Title 'Waiver Order' -FontColor 'White' -Content {
                             New-UDTable -Headers 'Manager' -Style 'centered' -Content {
                                 $Cache:FreakWaiver | Out-UDTableData -Property Manager
                             }
@@ -42,17 +42,17 @@ New-UDPage -Name 'Trades' -Icon exchange_alt -Endpoint {
                 }
                 New-UDTab -Text 'Vermin' -Content {
                     New-UDCollapsible -Items {
-                        New-UDCollapsibleItem -Title 'Open Offers' -Content {
+                        New-UDCollapsibleItem -Title 'Open Offers' -FontColor 'White' -Content {
                             New-UDGrid -Headers @('Type', 'Offered', 'Requested', 'Offered by', 'Offered to', 'Offer date') -DateTimeFormat "D/M/YY, H:mm" -NoExport -NoFilter -Properties @('Type', 'PlayersOut', 'PlayersIn', 'OutManager', 'InManager', 'createdAt') -Endpoint {
                                 $Cache:VerminTrades | Where-Object Status -eq 'awaiting_response' | Out-UDGridData
                             }
                         }
-                        New-UDCollapsibleItem -Title 'History' -Content {
+                        New-UDCollapsibleItem -Title 'History' -FontColor 'White' -Content {
                             New-UDGrid -Headers @('Type', 'Offered', 'Requested', 'Offered by', 'Offered to', 'Offer date', 'Response date', 'Status') -DateTimeFormat "D/M/YY, H:mm" -NoExport -NoFilter -Properties @('Type', 'PlayersOut', 'PlayersIn', 'OutManager', 'InManager', 'createdAt', 'respondedAt', 'Status') -Endpoint {
                                 $Cache:VerminTrades | Where-Object Status -ne 'awaiting_response' | Out-UDGridData
                             }
                         }
-                        New-UDCollapsibleItem -Title 'Waiver Order' -Content {
+                        New-UDCollapsibleItem -Title 'Waiver Order' -FontColor 'White' -Content {
                             New-UDTable -Headers 'Manager' -Style 'centered' -Content {
                                 $Cache:VerminWaiver | Out-UDTableData -Property Manager
                             }

74f77b12d0b00ea6f3c2fc8fb96ca24055237ccf
sk82jack <sk82jack@users.noreply.github.com>
2019-11-06 22:33:17 +0000

    change some text on cup page

--- Pages/cup.ps1
+++ Pages/cup.ps1
@@ -1,10 +1,10 @@
 New-UDPage -Name 'Cup' -Icon trophy -Endpoint {
     New-UDRow -Columns {
         New-UDColumn -SmallSize 12 -MediumSize 12 -LargeSize 8 -LargeOffset 2 -Endpoint {
-            New-UDTable -Title "Round $([int]$Cache:CurrentGameweek - 5) Gameweek Rankings" -Headers @("Manager", "Gameweek Points") -Endpoint {
+            New-UDTable -Title "Round $([int]$Cache:CurrentGameweek - 5) Cup Rankings" -Headers @("Manager", "Gameweek Points") -Endpoint {
                 $Cache:CupInfo[[int]$Cache:CurrentGameweek] | Out-UDTableData -Property @("Manager", "Gameweekpoints")
             }
-            New-UDTable -Title "Cup History" -Headers @("Round", "Eliminated", "Gameweek Rankings") -Endpoint {
+            New-UDTable -Title "Cup History" -Headers @("Round", "Eliminated", "Round Rankings") -Endpoint {
                 $Output = foreach ($Gameweek in $Cache:CupInfo.Keys) {
                     if ($Gameweek -eq [int]$Cache:CurrentGameweek) {
                         continue
@@ -18,18 +18,19 @@ New-UDPage -Name 'Cup' -Icon trophy -Endpoint {
                         $Eliminated = 'N/A'
                     }
 
+                    $RoundNumber = $Gameweek - 5
                     $ViewRankingsEndpoint = New-UDEndpoint -ArgumentList $Cache:CupInfo[$Gameweek] -Endpoint {
                         Show-UDModal -Content {
-                            New-UDTable -Title "Round $($Gameweek - 5) Gameweek Rankings" -Headers @("Manager", "Gameweek Points") -Content {
+                            New-UDTable -Title "Round $RoundNumber Cup Rankings" -Headers @("Manager", "Gameweek Points") -Content {
                                 $ArgumentList | Out-UDTableData -Property @("Manager", "Gameweekpoints")
                             }
                         }
                     }
 
                     [PSCustomObject]@{
-                        Round        = $Gameweek - 5
+                        Round        = $RoundNumber
                         Eliminated   = $Eliminated
-                        ViewRankings = New-UDButton -Text "View Gameweek Rankings" -OnClick $ViewRankingsEndpoint
+                        ViewRankings = New-UDButton -Text "Round $RoundNumber Rankings" -OnClick $ViewRankingsEndpoint
                     }
                 }
 

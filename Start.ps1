5f604cc478d8800f95b92f71cfe2d117ef001dc2
Jack Denton <JNDenton0606@gmail.com>
2020-05-27 03:32:26 +0100

    move certificate to nginx proxy

--- Start.ps1
+++ Start.ps1
@@ -1,6 +1,5 @@
 # UD Start Script
-Import-Module $PSScriptRoot/DraftFantasyFootballDashboard.psd1 -Force
+Import-Module .\DraftFantasyFootballDashboard.psd1 -Force
 
 Get-UDDashboard | Stop-UDDashboard
-openssl pkcs12 -export -out $PSScriptRoot/certificate.pfx -inkey /var/cert/private.key -in /var/cert/certificate.crt -certfile /var/cert/ca_bundle.crt -passout pass:
-Start-DashBoard -CertificatePath $PSScriptRoot/certificate.pfx
+Start-DashBoard

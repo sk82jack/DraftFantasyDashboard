c7ebf82ba24c0061e247f2def1261542add42d52
Jack Denton <JNDenton0606@gmail.com>
2020-05-27 04:09:17 +0100

    use $PSScriptRoot instead of .

--- Start.ps1
+++ Start.ps1
@@ -1,5 +1,5 @@
 # UD Start Script
-Import-Module .\DraftFantasyFootballDashboard.psd1 -Force
+Import-Module $PSScriptRoot/DraftFantasyFootballDashboard.psd1 -Force
 
 Get-UDDashboard | Stop-UDDashboard
 Start-DashBoard

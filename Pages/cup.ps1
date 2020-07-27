149e241823ba92911cc2b5e9687b6cd87a9d620b
Jack Denton <JNDenton0606@gmail.com>
2020-07-27 02:24:20 +0100

    Add cup winner to cup page if cup has been won

--- Pages/cup.ps1
+++ Pages/cup.ps1
@@ -1,11 +1,20 @@
 New-UDPage -Name 'Cup' -Icon trophy -Endpoint {
     New-UDRow -Columns {
         New-UDColumn -SmallSize 12 -MediumSize 12 -LargeSize 8 -LargeOffset 2 -Endpoint {
-            New-UDTable -Title "Round $([int]$Cache:CurrentGameweek - 5) Cup Rankings" -Headers @("Manager", "Round Points") -Endpoint {
-                $Cache:CupInfo[[int]$Cache:CurrentGameweek] | Out-UDTableData -Property @("Manager", "Gameweekpoints")
+            $Cache:AllCupRounds = [int[]]$Cache:CupInfo.Keys
+            $CupRounds = $Cache:AllCupRounds | Measure-Object -Maximum -Minimum
+            if ($Cache:CupInfo[[int]$CupRounds.Maximum].Winner) {
+                New-UDCard -Title 'CUP WINNER' -Size 'small' -FontColor 'White' -TextSize Large -Text $Cache:CupInfo[[int]$CupRounds.Maximum].Manager -TextAlignment 'center'
+                $Cache:AllCupRounds = $Cache:AllCupRounds -ne $CupRounds.Maximum
             }
+            else {
+                New-UDTable -Title "Round $([int]$Cache:CurrentGameweek - 5) Cup Rankings" -Headers @("Manager", "Round Points") -Endpoint {
+                    $Cache:CupInfo[[int]$Cache:CurrentGameweek] | Out-UDTableData -Property @("Manager", "Gameweekpoints")
+                }
+            }
+
             New-UDTable -Title "Cup History" -Headers @("Round", "Eliminated", "Round Rankings") -Endpoint {
-                $Output = foreach ($Gameweek in $Cache:CupInfo.Keys) {
+                $Output = foreach ($Gameweek in $Cache:AllCupRounds) {
                     if ($Gameweek -eq [int]$Cache:CurrentGameweek) {
                         continue
                     }

bdd057c07f6797afaec6a72454aeeb78b7fbe8b3
Jack Denton <JNDenton0606@gmail.com>
2020-08-07 00:17:39 +0100

    add history import function

---
+++ Modules/DraftFantasyFootball/Public/Import-DraftData.ps1
@@ -0,0 +1,41 @@
+function Import-DraftData {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory,
+            ParameterSetName = 'League'
+        )]
+        [ValidateSet('Prem', 'Freak', 'Vermin')]
+        [string]
+        $League,
+
+        [Parameter(
+            Mandatory,
+            ParameterSetName = 'Cup'
+        )]
+        [switch]
+        $Cup,
+
+        [Parameter(
+            Mandatory,
+            ParameterSetName = 'League'
+        )]
+        [ValidateSet('H2H', 'Picks', 'Standings')]
+        [string]
+        $DataType,
+
+        [Parameter(Mandatory)]
+        [int]
+        $Year
+    )
+
+    $ImportFolder = Join-Path -Path $PSScriptRoot -ChildPath "..\Data\$Year"
+
+    if ($League) {
+        $Path = Join-Path -Path $ImportFolder -ChildPath "League$DataType$League.xml"
+    }
+    elseif ($Cup) {
+        $Path = Join-Path -Path $ImportFolder -ChildPath "LeagueCup.xml"
+    }
+    Import-CliXml -Path $Path
+}

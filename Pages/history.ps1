2d344db977743aa5f7a5c3936f64fe8e80a74dfd
Jack Denton <JNDenton0606@gmail.com>
2020-08-08 13:44:39 +0100

    add Points by Draft Order to history page

--- Pages/history.ps1
+++ Pages/history.ps1
@@ -2,7 +2,7 @@ New-UDPage -Name 'History' -Endpoint {
     New-UDRow -Columns {
         New-UDColumn -SmallSize 12 -MediumSize 12 -LargeSize 8 -LargeOffset 2 -Endpoint {
             New-UDCollapsible -Items {
-                New-UDCollapsibleItem -Title '2019' -FontColor 'White' -Content {
+                New-UDCollapsibleItem -Title '2019 League Standings' -FontColor 'White' -Content {
                     New-UDTabContainer -Tabs {
                         New-UDTab -Text 'Prem' -Content {
                             New-UDTable -Headers @('Pos', 'Manager', 'P', 'W', 'D', 'L', 'S', 'Pts') -Style 'highlight' -Content {
@@ -21,7 +21,7 @@ New-UDPage -Name 'History' -Endpoint {
                         }
                     }
                 }
-                New-UDCollapsibleItem -Title '2018' -FontColor 'White' -Content {
+                New-UDCollapsibleItem -Title '2018 League Standings' -FontColor 'White' -Content {
                     New-UDTabContainer -Tabs {
                         New-UDTab -Text 'Prem' -Content {
                             New-UDTable -Headers @('Pos', 'Manager', 'P', 'W', 'D', 'L', 'S', 'Pts') -Style 'highlight' -Content {
@@ -35,7 +35,7 @@ New-UDPage -Name 'History' -Endpoint {
                         }
                     }
                 }
-                New-UDCollapsibleItem -Title '2017' -FontColor 'White' -Content {
+                New-UDCollapsibleItem -Title '2017 League Standings' -FontColor 'White' -Content {
                     New-UDTabContainer -Tabs {
                         New-UDTab -Text 'Prem' -Content {
                             New-UDTable -Headers @('Pos', 'Manager', 'P', 'W', 'D', 'L', 'S', 'Pts') -Style 'highlight' -Content {
@@ -47,6 +47,57 @@ New-UDPage -Name 'History' -Endpoint {
                                 Import-DraftData -League 'Freak' -DataType 'Standings' -Year 2017 | Out-UDTableData -Property @('Position', 'Manager', 'Played', 'Won', 'Drawn', 'Lost', 'Totalscore', 'Points')
                             }
                         }
+                    }
+                }
+                New-UDCollapsibleItem -Title 'Points by Draft Order' -FontColor 'White' -Content {
+                    New-UdChart -Title "Points by Draft Order" -Type Line -Endpoint {
+                        $Leagues = @{
+                            'Prem 2019'   = Import-DraftData -League Prem -Year 2019 -DataType Picks
+                            'Prem 2018'   = Import-DraftData -League Prem -Year 2018 -DataType Picks
+                            'Prem 2017'   = Import-DraftData -League Prem -Year 2017 -DataType Picks
+                            'Freak 2019'  = Import-DraftData -League Freak -Year 2019 -DataType Picks
+                            'Freak 2018'  = Import-DraftData -League Freak -Year 2018 -DataType Picks
+                            'Freak 2017'  = Import-DraftData -League Freak -Year 2017 -DataType Picks
+                            'Vermin 2019' = Import-DraftData -League Vermin -Year 2019 -DataType Picks
+                        }
+
+                        $ChartData = foreach ($Pick in 1..12) {
+                            $PickData = @{
+                                Pick    = $Pick
+                                Average = $null
+                            }
+
+                            foreach ($League in $Leagues.Keys) {
+                                $Round1CurrentPick = $Leagues[$League].Where{$_.Round -eq 1 -and $_.PickNo -eq $Pick}
+
+                                if (-not $Round1CurrentPick) {
+                                    continue
+                                }
+
+                                $LeagueName, $Year = $League.split()
+                                $Managers = $Leagues[$League] | Group-Object Manager -AsHashTable -AsString
+                                $PickData[$League] = ($Managers[$Round1CurrentPick.Manager].PlayerPoints | Measure-Object -Sum).Sum
+                            }
+                            [pscustomobject]$PickData
+                        }
+
+                        foreach ($Object in $ChartData) {
+                            $ObjectStats = $Object.psobject.Properties.Where{$_.Name -ne 'Pick'}.Value | Measure-Object -Sum
+                            $Object.Average = $ObjectStats.Sum / $ObjectStats.Count
+                        }
+
+                        $ChartData | Sort-Object Pick | Out-UDChartData -LabelProperty Pick -Dataset @(
+                            foreach ($League in $Leagues.Keys) {
+                                $Colour = switch -Regex ($League) {
+                                    'Prem' { 'Green' }
+                                    'Freak' { 'Orange' }
+                                    'Vermin' { 'Red' }
+                                }
+                                New-UdChartDataset -DataProperty $League -AdditionalOptions @{fill = $false} -Label $League -BackgroundColor $Colour -BorderColor $Colour
+                            }
+
+                            New-UdChartDataset -DataProperty 'Average' -AdditionalOptions @{fill = $false} -Label 'Average' -BackgroundColor 'Blue' -BorderColor 'Blue'
+                        )
                     }
                 }
             }

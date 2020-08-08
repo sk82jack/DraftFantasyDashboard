218901f3b140cd6f834b3232f06299736c5a87a2
Jack Denton <JNDenton0606@gmail.com>
2020-08-08 02:06:15 +0100

    minor changes to iframe

--- Pages/charter.ps1
+++ Pages/charter.ps1
@@ -1,14 +1,16 @@
 New-UDPage -Name 'Charter' -Endpoint {
     New-UDRow -Columns {
         New-UDColumn -SmallSize 12 -MediumSize 12 -LargeSize 8 -LargeOffset 2 -Endpoint {
-            New-UdHtml -Markup "<iframe
-                    src='https://docs.google.com/document/d/1tNLMUNix_D2qVk-_Z7bb0nN6l5mcRHfw6liZzMt4lwY/pub?embedded=true'
-                        style=
-                            'background-color:white;
-                            border:none;
-                            width: 100%;
-                            min-height: 600px;'>
-                    </iframe>"
+            New-UdHtml -Markup @'
+<iframe
+    src='https://docs.google.com/document/d/e/2PACX-1vSusM4Q5LXlVtUn6m8oQ9oemdU2X2XRJQXhM4Xd2e7t4yAaKzdJmUIQTiR39BUT2a3yv4M8raQbCnR6/pub?embedded=true'
+    style='
+      background-color:white;
+      border:none;
+      width: 100%;'
+    height="1380"
+</iframe>
+'@
         }
     }
 }

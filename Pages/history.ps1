1a634b31c8a3c41c613a1516864c4a6be777b40c
sk82jack <sk82jack@users.noreply.github.com>
2021-07-22 14:28:33 +0100

    add 2020 leage standings

--- Pages/history.ps1
+++ Pages/history.ps1
@@ -2,6 +2,25 @@ New-UDPage -Name 'History' -Endpoint {
     New-UDRow -Columns {
         New-UDColumn -SmallSize 12 -MediumSize 12 -LargeSize 8 -LargeOffset 2 -Endpoint {
             New-UDCollapsible -Items {
+                New-UDCollapsibleItem -Title '2020 League Standings' -FontColor 'White' -Content {
+                    New-UDTabContainer -Tabs {
+                        New-UDTab -Text 'Prem' -Content {
+                            New-UDTable -Headers @('Pos', 'Manager', 'P', 'W', 'D', 'L', 'S', 'Pts') -Style 'highlight' -Content {
+                                Import-DraftData -League 'Prem' -DataType 'Standings' -Year 2020 | Out-UDTableData -Property @('Position', 'Manager', 'Played', 'Won', 'Drawn', 'Lost', 'Totalscore', 'Points')
+                            }
+                        }
+                        New-UDTab -Text 'Freak' -Content {
+                            New-UDTable -Headers @('Pos', 'Manager', 'P', 'W', 'D', 'L', 'S', 'Pts') -Style 'highlight' -Content {
+                                Import-DraftData -League 'Freak' -DataType 'Standings' -Year 2020 | Out-UDTableData -Property @('Position', 'Manager', 'Played', 'Won', 'Drawn', 'Lost', 'Totalscore', 'Points')
+                            }
+                        }
+                        New-UDTab -Text 'Vermin' -Content {
+                            New-UDTable -Headers @('Pos', 'Manager', 'P', 'W', 'D', 'L', 'S', 'Pts') -Style 'highlight' -Content {
+                                Import-DraftData -League 'Vermin' -DataType 'Standings' -Year 2020 | Out-UDTableData -Property @('Position', 'Manager', 'Played', 'Won', 'Drawn', 'Lost', 'Totalscore', 'Points')
+                            }
+                        }
+                    }
+                }
                 New-UDCollapsibleItem -Title '2019 League Standings' -FontColor 'White' -Content {
                     New-UDTabContainer -Tabs {
                         New-UDTab -Text 'Prem' -Content {

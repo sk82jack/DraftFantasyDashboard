4baae64f529cbe20a7badb02965e18eefc0112e5
Jack Denton <JNDenton0606@gmail.com>
2021-11-07 02:41:22 +0000

    re-arrange stats page

--- Pages/stats.ps1
+++ Pages/stats.ps1
@@ -2,15 +2,23 @@ New-UDPage -Name 'Stats' -Endpoint {
     New-UDRow -Columns {
         New-UDColumn -SmallSize 12 -MediumSize 12 -LargeSize 8 -LargeOffset 2 -Endpoint {
             New-UDTabContainer -Tabs {
-                New-UDTab -Text 'Current Gameweek Stats' -Content {
-                    New-UDChart -Title 'Total League Points' -Type Bar -Endpoint {
+                New-UDTab -Text 'Current Season Stats' -Content {
+                    New-UDChart -Title 'Total League Points by League' -Type Bar -Endpoint {
                         $Sort = 'Prem', 'Freak', 'Vermin', 'Plankton'
                         $Cache:Tables.Keys | ForEach-Object {
                             [PSCustomObject]@{
-                                League       = $_
-                                WeeklyPoints = ($Cache:Tables[$_].WeeklyScore | Measure-Object -Sum).Sum
+                                League      = $_
+                                TotalPoints = ($Cache:Tables[$_].TotalScore | Measure-Object -Sum).Sum
                             }
-                        } | Sort-Object {$Sort.IndexOf($_.League)} | Out-UDChartData -DataProperty WeeklyPoints -LabelProperty League -BackgroundColor '#FF530D' -BorderColor 'black' -HoverBackgroundColor '#FF9F0D'
+                        } | Sort-Object {$Sort.IndexOf($_.League)} | Out-UDChartData -DataProperty TotalPoints -LabelProperty League -BackgroundColor '#FF530D' -BorderColor 'black' -HoverBackgroundColor '#FF9F0D'
+                    }
+
+                    New-UDChart -Title 'Top 10 Total League Points' -Type HorizontalBar -Endpoint {
+                        $Cache:Tables.Values | ForEach-Object {$_} | Sort-Object -Descending TotalScore | Select-Object -First 10 | Out-UDChartData -DataProperty TotalScore -LabelProperty Manager -BackgroundColor '#FF530D' -BorderColor 'black' -HoverBackgroundColor '#FF9F0D'
+                    }
+
+                    New-UDChart -Title 'Top 10 Total League Points Against' -Type HorizontalBar -Endpoint {
+                        $Cache:Tables.Values | ForEach-Object {$_} | Sort-Object -Descending TotalScoreAgainst | Select-Object -First 10 | Out-UDChartData -DataProperty TotalScoreAgainst -LabelProperty Manager -BackgroundColor '#FF530D' -BorderColor 'black' -HoverBackgroundColor '#FF9F0D'
                     }
 
                     New-UDChart -Title 'Top 10 Weekly League Points' -Type HorizontalBar -Endpoint {
@@ -35,23 +43,15 @@ New-UDPage -Name 'Stats' -Endpoint {
                         $StructuredPoints | Sort-Object -Descending WeeklyPoints | Select-Object -First 10 | Out-UDChartData -DataProperty WeeklyPoints -LabelProperty Manager -BackgroundColor '#FF530D' -BorderColor 'black' -HoverBackgroundColor '#FF9F0D'
                     }
                 }
-                New-UDTab -Text 'Current Season Stats' -Content {
-                    New-UDChart -Title 'Total League Points' -Type Bar -Endpoint {
+                New-UDTab -Text 'Current Gameweek Stats' -Content {
+                    New-UDChart -Title 'Total League Points by League' -Type Bar -Endpoint {
                         $Sort = 'Prem', 'Freak', 'Vermin', 'Plankton'
                         $Cache:Tables.Keys | ForEach-Object {
                             [PSCustomObject]@{
-                                League      = $_
-                                TotalPoints = ($Cache:Tables[$_].TotalScore | Measure-Object -Sum).Sum
+                                League       = $_
+                                WeeklyPoints = ($Cache:Tables[$_].WeeklyScore | Measure-Object -Sum).Sum
                             }
-                        } | Sort-Object {$Sort.IndexOf($_.League)} | Out-UDChartData -DataProperty TotalPoints -LabelProperty League -BackgroundColor '#FF530D' -BorderColor 'black' -HoverBackgroundColor '#FF9F0D'
-                    }
-
-                    New-UDChart -Title 'Top 10 Individual League Points' -Type HorizontalBar -Endpoint {
-                        $Cache:Tables.Values | ForEach-Object {$_} | Sort-Object -Descending TotalScore | Select-Object -First 10 | Out-UDChartData -DataProperty TotalScore -LabelProperty Manager -BackgroundColor '#FF530D' -BorderColor 'black' -HoverBackgroundColor '#FF9F0D'
-                    }
-
-                    New-UDChart -Title 'Top 10 Individual League Points Against' -Type HorizontalBar -Endpoint {
-                        $Cache:Tables.Values | ForEach-Object {$_} | Sort-Object -Descending TotalScoreAgainst | Select-Object -First 10 | Out-UDChartData -DataProperty TotalScoreAgainst -LabelProperty Manager -BackgroundColor '#FF530D' -BorderColor 'black' -HoverBackgroundColor '#FF9F0D'
+                        } | Sort-Object {$Sort.IndexOf($_.League)} | Out-UDChartData -DataProperty WeeklyPoints -LabelProperty League -BackgroundColor '#FF530D' -BorderColor 'black' -HoverBackgroundColor '#FF9F0D'
                     }
                 }
             }
